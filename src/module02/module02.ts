30ae889607785102c74fbfe2f2518593a55686de
hanool <29415530+hanool@users.noreply.github.com>
2023-02-08 09:12:51 +0900

    add module2 with module2.ts

---
+++ src/module02/module02.ts
@@ -0,0 +1,12 @@
+let x: number;	// Explicitly declares x as a number type
+let y = 1;		// Implicitly declares y as a number type
+let z;			// Declares z without initializing it
+
+x = 1;
+x = "one";
+
+y = "one";
+
+z = 1;
+z = "one";
+
